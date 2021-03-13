b62b94d43f9cc54d3e8270c7b1603972f1cae467
Megan Kirkbride <megan.kirkbride@bloomerang.co>
2021-03-13 18:12:15 -0500

    Add about page #12

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Page from "../components/styled/Page";
+
+const About = () => {
+  return <Page>
+    <h2>About Made by Melanie</h2>
+    <p>
+      I enjoy making homemade items and crafts! I love
+      making personalized designs so please visit the Contact
+      page to send me a message about any custom designs. 
+    </p>
+  </Page>;
+};
+
+export default About;
