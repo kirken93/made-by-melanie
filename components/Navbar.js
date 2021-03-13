cbcddc6ee979f820ca7e46ec8e892bc127082784
Megan Kirkbride <megan.kirkbride@bloomerang.co>
2021-03-13 18:12:15 -0500

    Add about page #12

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,6 +18,15 @@ const NavContainer = styled.div`
   align-items: center;
 `;
 
+const NavItems = styled.div`
+  display: flex;
+  align-items:center;
+`;
+
+const NavItem = styled.div`
+  padding: 1rem;
+`;
+
 const ShoppingCart = styled(FiShoppingCart)`
   margin: 1rem;
   cursor: pointer;
@@ -29,12 +38,21 @@ const Navbar = () => {
 
   return <Nav>
     <NavContainer>
-      <Link href="/">
-        <UnstyledLink>
-          <img src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/51088746_2219450884981397_8151053238599155712_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=oqetLuzZbD4AX_GZeF1&_nc_ht=scontent-ort2-1.xx&oh=5e04ffb8a4455066b6f905ba30616af5&oe=6072D36C"
-               height={150} />
-        </UnstyledLink>
-      </Link>
+      <NavItems>
+        <NavItem>
+          <Link href="/">
+            <UnstyledLink>
+              <img src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/51088746_2219450884981397_8151053238599155712_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=oqetLuzZbD4AX_GZeF1&_nc_ht=scontent-ort2-1.xx&oh=5e04ffb8a4455066b6f905ba30616af5&oe=6072D36C"
+                   height={150} />
+            </UnstyledLink>
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/about">
+            <UnstyledLink>About</UnstyledLink>
+          </Link>
+        </NavItem>
+      </NavItems>
       <ShoppingCart onClick={openCart} />
     </NavContainer>
   </Nav>;
